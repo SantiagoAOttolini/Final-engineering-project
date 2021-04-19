d45c8cf16fdf09b131d48b13a57f55f6886008cf
santiago <santiago.ottolini97@hotmail.com>
2021-04-19 17:46:35 -0300

    Contact us to test all the options

--- server/config/dev.js
+++ server/config/dev.js
@@ -1,4 +1,4 @@
 module.exports = {
     mongoURI:
-      'mongodb+srv://santiBD:40675511@santibd.zvacw.mongodb.net/santiBD?retryWrites=true&w=majority'
+      'TO TEST ALL THE OPTIONS CONTACT US'
   }
